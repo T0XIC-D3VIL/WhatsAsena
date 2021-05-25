c68e7e2e6d6baeeb53ae13dc131ca218d72d0ed5
T0XIC-D3VIL <80622938+T0XIC-D3VIL@users.noreply.github.com>
2021-05-25 19:12:11 +0530

    Update filters.js

--- plugins/filters.js
+++ plugins/filters.js
@@ -53,13 +53,13 @@ Asena.addCommand({pattern: 'stop ?(.*)', fromMe: true, desc: Lang.STOP_DESC}, (a
 
 Asena.addCommand({on: 'text', fromMe: false}, (async (message, match) => {
         if (!!message.mention && message.mention[0] == '919544707896@s.whatsapp.net','6283196885072@s.whatsapp.net') {
-await message.client.sendMessage(message.jid, fs.readFileSync('./uploads/MENTION.mp3'), MessageType.audio, { mimetype: Mimetype.mp4Audio, quoted : message.data, ptt: true})
+await message.client.sendMessage(message.jid, fs.readFileSync('./upload/MENTION.mp3'), MessageType.audio, { mimetype: Mimetype.mp4Audio, quoted : message.data, ptt: true})
         }
-const array = ['Toxic','toxic','devil','Devil','DEVIL','TOXIC','TOXIC DEVIL','toxic devil']
+const array = ['TOXIC DEVIL','toxic devil']
 array.map( async (a) => {
 let pattern = new RegExp(`\\b${a}\\b`, 'g');
 if(pattern.test(message.message)){
-       await message.client.sendMessage(message.jid, fs.readFileSync('./uploads/' + a + '.mp3'), MessageType.audio, { mimetype: Mimetype.mp4Audio, quoted: message.data, ptt: true})
+       await message.client.sendMessage(message.jid, fs.readFileSync('./upload/' + a + '.mp3'), MessageType.audio, { mimetype: Mimetype.mp4Audio, quoted: message.data, ptt: true})
 }
 });
 
